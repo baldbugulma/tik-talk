1771e8abb7d9fbe97b81c32c4e818cd2121d814d
baldbugulma <ForZaArindal@gmail.com>
2024-11-01 13:19:21 +0300

    tik-talk lesson 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileCardComponent } from "./common-ui/profile-card/profile-card.component";
+import { Profile } from './data/services/interfaces/profile.interface';
+import { ProfileService } from './data/services/profile.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileCardComponent, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tik-talk';
+  profileService :ProfileService = inject(ProfileService)
+  profiles: Profile[] = []
+
+  constructor(){
+    this.profileService.getTestAccounts()
+    .subscribe(val => {
+      this.profiles = val;
+    })
+  }
+
 }
