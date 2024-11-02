ea96478221ecf46668c02c31d8a65435916d9e73
baldbugulma <ForZaArindal@gmail.com>
2024-11-02 14:43:46 +0300

    get_token_auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { JsonPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from "./common-ui/profile-card/profile-card.component";
-import { Profile } from './data/services/interfaces/profile.interface';
-import { ProfileService } from './data/services/profile.service';
 
 @Component({
   selector: 'app-root',
@@ -13,14 +11,5 @@ import { ProfileService } from './data/services/profile.service';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  profileService :ProfileService = inject(ProfileService)
-  profiles: Profile[] = []
-
-  constructor(){
-    this.profileService.getTestAccounts()
-    .subscribe(val => {
-      this.profiles = val;
-    })
-  }
 
 }
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  authService = inject(AuthService)
+
+  form = new FormGroup<{ 
+    username: FormControl<null | string>; 
+    password: FormControl<null | string>; 
+  }>({
+    username: new FormControl <null | string>(null, Validators.required),
+    password: new FormControl <null | string>(null, Validators.required)
+  });
+
+  onSubmit(){
+    if(this.form.valid){
+      console.log(this.form.value)
+      //@ts-ignore
+        this.authService.login(this.form.value).subscribe()
+    }
+
+  };
 
 }
