ffd225e59f044b1120fb96fd9d7459eb9a8b6e2f
baldbugulma <ForZaArindal@gmail.com>
2024-11-07 16:50:09 +0300

    add default avatarUrl

--- src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
+++ src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
@@ -1,17 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
 import { DndDirective } from '../../../common-ui/directives/dnd.directive';
 import { SvgIconComponent } from "../../../common-ui/svg-icon/svg-icon.component";
+import { ProfileService } from '../../../data/services/profile.service';
 
 @Component({
   selector: 'app-avatar-upload',
   standalone: true,
-  imports: [SvgIconComponent, DndDirective],
+  imports: [SvgIconComponent, DndDirective, AsyncPipe],
   templateUrl: './avatar-upload.component.html',
   styleUrl: './avatar-upload.component.scss'
 })
 export class AvatarUploadComponent {
+
+  profileService = inject(ProfileService)
+
+  previewUrl: string | null | undefined = this.profileService.me()?.avatarUrl ? this.profileService.me()?.avatarUrl : `/assets/imgs/avatar-placeholder.png`
+
+  preview = signal<string>(`${this.previewUrl}`)
   
-  preview = signal<string>('/assets/imgs/avatar-placeholder.png')
 
   fileBrowserHandler(event: Event){
       const file :File | undefined = (event.target as HTMLInputElement)?.files?.[0]
